ada4fbebbc09eae21c89836c7c0e72032adc235d
Mai Xuân Hiếu <02hieu02@gmail.com>
2023-11-29 15:05:49 +0700

    fix not null

--- BTTH01_CSE485.sql
+++ BTTH01_CSE485.sql
@@ -3,25 +3,25 @@ CREATE DATABASE BTTH01_CSE485
 USE BTTH01_CSE485
 
 CREATE TABLE tacgia(
-		ma_tgia INT UNSIGNED PRIMARY KEY,
-		ten_tgia VARCHAR(100),
+		ma_tgia INT UNSIGNED PRIMARY KEY NOT NULL ,
+		ten_tgia VARCHAR(100) NOT NULL,
 		hinh_tgia VARCHAR(1000)
 );
 CREATE TABLE theloai(
-		ma_tloai INT UNSIGNED PRIMARY KEY,
-		ten_tloai VARCHAR(50)
+		ma_tloai INT UNSIGNED PRIMARY KEY NOT NULL,
+		ten_tloai VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE baiviet(
-		ma_bviet INT UNSIGNED PRIMARY KEY,
-		tieude VARCHAR(200),
-		ten_bhat VARCHAR(200),
-		ma_tloai INT UNSIGNED,
-		tomtat TEXT,
+		ma_bviet INT UNSIGNED PRIMARY KEY NOT NULL,
+		tieude VARCHAR(200) NOT NULL ,
+		ten_bhat VARCHAR(200) NOT NULL,
+		ma_tloai INT UNSIGNED NOT NULL,
+		tomtat TEXT NOT NULL,
 		noidung TEXT,
-		ma_tgia INT UNSIGNED,
-		ngayviet DATETIME DEFAULT CURRENT_TIMESTAMP,
+		ma_tgia INT UNSIGNED NOT NULL,
+		ngayviet DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
 		hinhanh VARCHAR(200),
 		FOREIGN KEY (ma_tloai) REFERENCES theloai(ma_tloai),
 		FOREIGN KEY (ma_tgia) REFERENCES tacgia(ma_tgia)
